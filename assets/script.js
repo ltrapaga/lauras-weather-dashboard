bbf92d9584d6398b69043bfdc156a768482759d9
Laura Trapaga <trapaga9@gmail.com>
2022-12-02 17:12:24 -0500

    Add HTML code. Add variables to JS code.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,11 @@
 // Variables
+var nameEl = document.getElementById("city-name");
+var historyEl = document.getElementById("history");
+var inputEl = document.getElementById("city-input");
+var clearEl = document.getElementById("clear-history");
+var searchEl = document.getElementById("search-button");
+var currentWindEl = document.getElementById("wind-speed");
+var currentPicEl = document.getElementById("current-pic");
+var currentHumidityEl = document.getElementById("humidity");
+var currentTemperatureEl = document.getElementById("temperature");
+var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
